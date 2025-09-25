cef15332fb8983d919c46085afc8d70f64014c7b
pradyot29 <sonipradyot@gmail.com>
2025-09-25 16:00:06 +0530

    change in frontend baseURL

--- frontend/jewelry-shop-frontend/src/services/api.js
+++ frontend/jewelry-shop-frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 const api = axios.create({
-  baseURL:  import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL:  API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
