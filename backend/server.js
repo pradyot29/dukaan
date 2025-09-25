078a51c3df3968cafedf7eb0c40f41976e5dcd6d
pradyot29 <sonipradyot@gmail.com>
2025-09-25 14:33:45 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,10 @@ const express = require('express');
      .then(() => console.log('Connected to MongoDB'))
      .catch(err => console.error('MongoDB connection error:', err));
 
-   const PORT = process.env.PORT || 3000;
-   app.listen(PORT, () => {
-     console.log(`Server running on port ${PORT}`);
-   });
+
+  if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
