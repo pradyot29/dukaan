13502cb6880b3b5ac20ee98a7d95b6e3991c34c2
pradyot29 <sonipradyot@gmail.com>
2025-09-26 12:45:56 +0530

    allow all origins

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ const express = require('express');
    // Middleware
    app.use(express.json());
   app.use(cors({
-  origin: [
-    "http://localhost:3001",               // local frontend
-    "https://dukaan-hisaab.vercel.app"  // deployed frontend
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
